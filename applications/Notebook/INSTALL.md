5d05757a65d53a817292efa9e94c473eaa43a490
Liu Kun <liukunup@163.com>
2023-03-06 21:36:13 +0800

    调整Notebook安装方式

--- applications/Notebook/INSTALL.md
+++ applications/Notebook/INSTALL.md
@@ -1,11 +1,17 @@
-# Jupyter Notebook
+# Notebook
+
+提供如下语言的`Notebook`部署方法
+
+- Python
+- C++
+- SQL
 
 ## 安装部署
 
 ### Docker
 
 ```shell
-sh deploy.sh
+bash deploy.sh
 ```
 
 ### Docker Compose
@@ -19,11 +25,32 @@ docker-compose up -d
 1. 创建命名空间
 
 ```shell
+# 如已创建, 请跳过
 kubectl create namespace homelab
 ```
 
 2. 部署或更新
 
 ```shell
-kubectl apply -f k8s -n homelab
+kubectl apply -f k8s-notebook-python.yaml -n homelab
+kubectl apply -f k8s-notebook-cpp.yaml -n homelab
+kubectl apply -f k8s-notebook-sql.yaml -n homelab
 ```
+
+3. 卸载
+
+```shell
+kubectl delete -f k8s-notebook-xxx.yaml -n homelab
+```
+
+## 体验使用
+
+### 通过 `Docker` or `Docker Compose` 方式部署
+
+请访问`http://{ip}:{port}/`
+
+### 通过 `Kubernetes` 方式部署
+
+- [Python](http://py.notebook.homelab.com/) py.notebook.homelab.com
+- [C++](http://cpp.notebook.homelab.com/) cpp.notebook.homelab.com
+- [SQL](http://sql.notebook.homelab.com/) sql.notebook.homelab.com
--- applications/Notebook/deploy.sh
+++ applications/Notebook/deploy.sh
@@ -1,12 +1,38 @@
 #! /bin/bash
-# author : Liu Kun
-# date   : 2022-11-20 21:00:00
+# author   : Liu Kun
+# date     : 2023-03-06 21:24:00
+# url      : http://{host}:{port}/
+# password : 123456
 
+# Python
 docker run -d \
     -p 8888:8888 \
     -e DOCKER_STACKS_JUPYTER_CMD=lab \
+    -e JUPYTER_ENABLE_LAB=yes \
     -e GRANT_SUDO=yes \
     --restart=always \
-    --name=notebook \
-    jupyter/minimal-notebook:notebook-6.5.1 start-notebook.sh \
-    --NotebookApp.password='sha1:a7c0702d28e9:8a8868c5d4ea33af70e04c634487402b3997f40c'
+    --name=notebook-python \
+    jupyter/minimal-notebook:notebook-6.5.2 start-notebook.sh \
+    --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$LaE/ahJVX6KHFhkYvwPv9w$BN+q+SBvJlcFn41qEIUq2GMT/TapRZcoiPeJfmvyqdA'
+
+# C++
+docker run -d \
+    -p 8889:8888 \
+    -e DOCKER_STACKS_JUPYTER_CMD=lab \
+    -e JUPYTER_ENABLE_LAB=yes \
+    -e GRANT_SUDO=yes \
+    --restart=always \
+    --name=notebook-python \
+    datainpoint/xeus-cling-notebook:latest start-notebook.sh \
+    --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$LaE/ahJVX6KHFhkYvwPv9w$BN+q+SBvJlcFn41qEIUq2GMT/TapRZcoiPeJfmvyqdA'
+
+# SQL
+docker run -d \
+    -p 8890:8888 \
+    -e DOCKER_STACKS_JUPYTER_CMD=lab \
+    -e JUPYTER_ENABLE_LAB=yes \
+    -e GRANT_SUDO=yes \
+    --restart=always \
+    --name=notebook-python \
+    datainpoint/xeus-sql-notebook:latest start-notebook.sh \
+    --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$LaE/ahJVX6KHFhkYvwPv9w$BN+q+SBvJlcFn41qEIUq2GMT/TapRZcoiPeJfmvyqdA'
