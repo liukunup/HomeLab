6777c35a490a006672e27ce25d05b8c97ef1f1d5
LiuKun <liukunup@163.com>
2025-05-04 11:52:38 +0800

    修复缺陷

--- scripts/quick_gen_cert.sh
+++ scripts/quick_gen_cert.sh
@@ -3,9 +3,11 @@
 # version     : 1.2
 # date        : 2025-04-30
 # description : Create self-signed certificate
-# usage       : ./quick.sh <domain>
+# usage       : ./quick.sh <domain> [<ip-address>]
 
 domain=$1
+ip_address=$2
+
 if [ -z $domain ]; then
     read -p "Enter the domain (e.g. xxx.homelab.lan): " domain
     echo
@@ -13,6 +15,11 @@ if [ -z $domain ]; then
         echo "Must provide a domain to self-sign the certificate"
         exit 1
     fi
+fi
+
+if [ -z "$ip_address" ]; then
+    read -p "Enter the IP address (optional, press Enter to skip): " ip_address
+    echo
 fi
 
 echo "Creat self-signed certificate for domain: $domain"
@@ -24,46 +31,61 @@ openssl genrsa -out server/$domain/private/server.key 4096
 echo
 
 echo "2. Generating certificate signing request"
-openssl req -config <(printf "
-[req]
-prompt = no               # 禁用交互式提示
-distinguished_name = dn   # 指定用于 CSR 的“可分辨名称”部分
-req_extensions = req_ext  # 指定要应用的请求扩展部分
-
-[dn]
-C = CN        # 国家代码 (Country)
-ST = HuBei    # 州/省 (State or Province)
-L = WuHan     # 城市/地区 (Locality)
-O = Home      # 组织名称 (Organization)
-OU = HomeLab  # 组织单位名称 (Organizational Unit)
-CN = $domain  # 通用名称 (Common Name)，通常是域名或主机名
-
-[req_ext]
-subjectAltName = @alt_names
-
-[alt_names]
-DNS.1 = $domain      # 第一个 DNS 名称，例如：example.homelab.lan
-DNS.2 = gray.$domain # 第二个 DNS 名称，例如：gray.example.homelab.lan
-") -new -sha512 \
--key server/$domain/private/server.key \
--out server/$domain/csr/server.csr
+openssl req -new -sha512 \
+    -out server/$domain/csr/server.csr \
+    -key server/$domain/private/server.key \
+    -subj "/C=CN/ST=HuBei/L=WuHan/O=Home/OU=HomeLab/CN=$domain"
 echo
 
 echo "3. Generating self-signed certificate"
-echo -e "y\ny" | openssl ca -config intermediate/openssl.cnf \
-     -extensions server_cert -days 3650 -notext -md sha512 \
-     -in server/$domain/csr/server.csr \
-     -out server/$domain/certs/server.cert
+if [ -n "$ip_address" ]; then
+echo -e "y\ny" | openssl x509 -req \
+    -out server/$domain/certs/server.cert \
+    -in server/$domain/csr/server.csr \
+    -CA intermediate/certs/intermediate.crt \
+    -CAkey intermediate/private/intermediate.key \
+    -CAcreateserial -days 3650 -sha512 \
+    -extfile <(echo -e "
+      subjectKeyIdentifier = hash
+      authorityKeyIdentifier = keyid:always
+      basicConstraints = critical, CA:FALSE
+      keyUsage = digitalSignature, keyEncipherment
+      extendedKeyUsage = serverAuth
+      subjectAltName = DNS:$domain,IP:$ip_address
+    ")
+else
+echo -e "y\ny" | openssl x509 -req \
+    -out server/$domain/certs/server.cert \
+    -in server/$domain/csr/server.csr \
+    -CA intermediate/certs/intermediate.crt \
+    -CAkey intermediate/private/intermediate.key \
+    -CAcreateserial -days 3650 -sha512 \
+    -extfile <(echo -e "
+      subjectKeyIdentifier = hash
+      authorityKeyIdentifier = keyid:always
+      basicConstraints = critical, CA:FALSE
+      keyUsage = digitalSignature, keyEncipherment
+      extendedKeyUsage = serverAuth
+      subjectAltName = DNS:$domain
+    ")
+fi
 echo
 
 echo "4. Generating fullchain certificate"
-cat root/certs/ca.crt \
+echo "4.1 Generating fullchain.pem"
+cat server/$domain/certs/server.cert \
     intermediate/certs/intermediate.crt \
-    server/$domain/certs/server.cert \
     > server/$domain/certs/fullchain.pem
+echo "4.2 Generating server.p12"
+openssl pkcs12 -export \
+    -out server/$domain/certs/server.p12 \
+    -inkey server/$domain/private/server.key \
+    -in server/$domain/certs/server.cert \
+    -certfile intermediate/certs/intermediate.crt \
+    -name "$domain"
 echo
 
 echo "5. Verify certificate"
 openssl x509 -noout -text -in server/$domain/certs/server.cert
-openssl verify -CAfile intermediate/certs/fullchain.pem server/$domain/certs/fullchain.pem
+openssl verify -CAfile intermediate/certs/fullchain.pem server/$domain/certs/server.cert
 echo
