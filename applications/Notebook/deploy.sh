65720f72f522b1cdaa12da5c4062f3334992ca1a
LiuKun/Windows <liukunup@163.com>
2025-09-15 23:19:25 +0800

    修改命名错误

--- applications/Notebook/deploy.sh
+++ applications/Notebook/deploy.sh
@@ -22,7 +22,7 @@ docker run -d \
     -e JUPYTER_ENABLE_LAB=yes \
     -e GRANT_SUDO=yes \
     --restart=always \
-    --name=notebook-python \
+    --name=notebook-cpp \
     datainpoint/xeus-cling-notebook:latest start-notebook.sh \
     --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$LaE/ahJVX6KHFhkYvwPv9w$BN+q+SBvJlcFn41qEIUq2GMT/TapRZcoiPeJfmvyqdA'
 
@@ -33,6 +33,6 @@ docker run -d \
     -e JUPYTER_ENABLE_LAB=yes \
     -e GRANT_SUDO=yes \
     --restart=always \
-    --name=notebook-python \
+    --name=notebook-sql \
     datainpoint/xeus-sql-notebook:latest start-notebook.sh \
     --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$LaE/ahJVX6KHFhkYvwPv9w$BN+q+SBvJlcFn41qEIUq2GMT/TapRZcoiPeJfmvyqdA'
